421af0a19bd18f9bfe07c991b27599cd75b53625
LujanSolo <lujansolo@proton.me>
2023-05-28 22:36:01 -0700

    added initial styling, references, classes, and ids for menu items

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ let menu = document.getElementById('menu-container');
 menuArray.forEach((item) => {
   menu.innerHTML += `
     <div class="menu-item">
-      <i class="item-icon" alt="An icon representing ${item.name}">${item.emoji}</i>
-      <h2>${item.name}</h2>
-      <p>${item.ingredients}</p>
-      <h3>${item.price}</h3>
-      <button class="add-btn" id="add-btn">+</button>
+      <div class="menu-icon">
+        <i class="item-icon" alt="An icon representing ${item.name}">${item.emoji}</i>
+      </div>
+      <div class="menu-detail">  
+        <h2>${item.name}</h2>
+        <p class="ingredients">${item.ingredients}</p>
+        <h3>$${item.price}</h3>
+      </div>
+      <div class="add-item-btn">  
+        <button class="add-btn" id="add-btn">+</button>
+      </div>
     </div>
   `;
 });
