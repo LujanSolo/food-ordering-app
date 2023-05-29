ac38711f96ef02c50aecddb1c28da5bba6c1ff63
LujanSolo <lujansolo@proton.me>
2023-05-28 21:13:41 -0700

    menu items are now added from data.js via javascript forEach. import/export setup for data.js, as well as added 'module' to the html script

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import { menuArray } from './data'
+import { menuArray } from "./data.js";
+let menu = document.getElementById('menu-container');
+
+
+menuArray.forEach((item) => {
+  menu.innerHTML += `
+    <div class="menu-item">
+      <i class="item-icon" alt="An icon representing ${item.name}">${item.emoji}</i>
+      <h2>${item.name}</h2>
+      <p>${item.ingredients}</p>
+      <h3>${item.price}</h3>
+      <button class="add-btn" id="add-btn">+</button>
+    </div>
+  `;
+});
