db5d9a7e9dc83ab23089a52bb06b048cb78ccbb6
LujanSolo <lujansolo@proton.me>
2023-06-12 23:31:27 -0700

    added uuid via npm to project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { menuArray } from "./data.js";
+import { v4 as uuidv4 } from 'uuid';
+
 let currentOrderBucket = [];
 
 //* on page load, get any items from local storage and push to currentOrderBucket
@@ -32,24 +34,6 @@ function getMenuHtml() {
   return menuHtml;
 }
 
-function getOrderHtml() {
-  let orderHtml = "";
-
-  currentOrderBucket.forEach((orderItem) => {
-    orderHtml += `
-      <h3 class="order-header">Your Order</h3>
-      <div class="order-item">
-        <h2 class="order-col-1">${orderItem.name}</h2>
-        <button class="remove-btn" id="${orderItem.uuid}">remove</button>
-        <p class="item-price">${orderItem.price}</p>
-        <h3 class="order-col-1">Total price:</h3>
-        <p class="item-price" id="sum-price">price function</p>
-        <button class="order-btn" id="order-btn">Complete Order</button>
-      </div>
-      
-    `
-  })
-}
 
 //* FILTER the selected menu item, send to new array
 function getTargetObject(itemId) {
@@ -65,15 +49,34 @@ function pushOrderToArray(object) {
   currentOrderBucket.push(object);
   pushToLocalStorage(currentOrderBucket);
 }
-console.log(currentOrderBucket); //! DELETE LATER
 
 //* SAVE OrderObject to Local Storage
 function pushToLocalStorage(object) {
   localStorage.setItem("order", JSON.stringify(object));
 }
 
+//* FUNCTION to build the HTML for active orders
+function getOrderHtml() {
+  let orderHtml = "";
+
+  currentOrderBucket.forEach((orderItem) => {
+    orderHtml += `
+      <div class="order-item">
+        <h2 class="order-col-1">${orderItem.name}</h2>
+        <button class="remove-btn" id="${orderItem.id}">remove</button>
+        <p class="item-price">${orderItem.price}</p>
+        <h3 class="order-col-1">Total price:</h3>
+        <p class="item-price" id="sum-price">price function</p>
+        <button class="order-btn" id="order-btn">Complete Order</button>
+      </div>
+    `
+  });
+  return orderHtml;
+}
+console.log(currentOrderBucket)
 //* render current menu items to the page
 function render() {
   document.getElementById("menu-container").innerHTML = getMenuHtml();
+  // document.getElementById("order-details").innerHTML = getOrderHtml();
 }
 render();
