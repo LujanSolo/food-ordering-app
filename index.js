b667614e7daeef1dfd9f0aa0171d2afe454573eb
LujanSolo <lujansolo@proton.me>
2023-06-14 13:32:03 -0700

    added orderObj to addItemToOrderArray function in order to sift out the key/value pairs that I want in the yourOrder fields

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 import { menuArray } from "./data.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid"
 
-let currentOrderBucket = [];
-
-//* Document event listener using Event Object for targeting
-document.addEventListener("click", (e) => {
-  if (e.target.id) {
-    getTargetObject(e.target.id);
-  }
-});
-
 //* ITERATE thru menuArray to display each menu item to the HTML
 function getMenuHtml() {
   let menuHtml = "";
@@ -27,69 +18,87 @@ function getMenuHtml() {
           <button class="add-btn" id="${item.id}">+</button>
         </div>
       </div>
-    `;
+    `
   });
   return menuHtml;
-}
-
-//* FILTER the selected menu item, send to new array
-function getTargetObject(itemId) {
-  let targetMenuObj = menuArray.filter((item) => {
-    return item.id == itemId;
-  });
-  addItemToOrderArray(targetMenuObj)
-  console.log(currentOrderBucket)
-}
-
-//* PUSH selected OrderObject to the currentOrder array
-function addItemToOrderArray(object) {
-  object.uuid = uuidv4();
-  currentOrderBucket.unshift(object);
-  console.log('currentOrderBucket:', currentOrderBucket)
-  // pushToLocalStorage(currentOrderBucket);
-}
-
-//* SAVE OrderObject to Local Storage
-function pushToLocalStorage(object) {
-  localStorage.setItem("order", JSON.stringify(object));
-}
-
-//* GET OrderObject from Local Storage
-function getLocalStorage(object) {
-  currentOrderBucket = JSON.parse(localStorage.getItem("order"));
-}
-
-//* FUNCTION to build the HTML for active orders
-function getOrderHtml() {
-  let orderHtml = "";
-  
-  if(currentOrderBucket.length) {
-    currentOrderBucket.forEach((orderItem) => {
-      orderHtml += `
-        <div class="order-item">
-          <h2 class="order-col-1">${orderItem.name}</h2>
-          <button class="remove-btn" id="${orderItem.uuid}">remove</button>
-          <p class="item-price">${orderItem.price}</p>
-          <h3 class="order-col-1">Total price:</h3>
-          <p class="item-price" id="sum-price">price function</p>
-          <button class="order-btn" id="order-btn">Complete Order</button>
-        </div>
-      `
-    });
-  }
-  
-  return orderHtml;
 }
 
 //* render current menu items to the page
 function renderMenu() {
   document.getElementById("menu-container").innerHTML = getMenuHtml();
-  
-}
-
-function renderOrder() {
-  document.getElementById("order-details").innerHTML = getOrderHtml();
 }
 
 renderMenu();
-renderOrder();
+
+
+//* ORDER SECTION
+
+
+
+//* Document event listener using Event Object for targeting
+document.addEventListener("click", (e) => {
+  if (e.target.id) {
+    getTargetObject(e.target.id);
+  }
+});
+
+//* FILTER the selected menu item, send to new array
+function getTargetObject(itemId) {
+  const targetMenuObj = menuArray.filter((item) => {
+  return item.id === Number(itemId)
+  })[0];
+  addItemToOrderArray(targetMenuObj);
+}
+
+
+let currentOrderArray = [];
+//* PUSH selected OrderObject to the currentOrder array
+function addItemToOrderArray(item) {
+  
+  const orderObj = {
+    name: item.name,
+    price: item.price,
+    id: item.id,
+    uuid: uuidv4()
+  }
+
+  currentOrderArray.push(orderObj);
+  renderOrder();
+  console.log(currentOrderArray)
+}
+
+
+//* FUNCTION to build the HTML for active orders
+function getOrderHtml() {
+  let orderHtml = "";
+
+  currentOrderArray.forEach((item) => {
+    orderHtml += `
+        <div class="order-item">
+          <h2 class="order-col-1">${item.name}</h2>
+          <button class="remove-btn" id="${item.uuid}">remove</button>
+          <p class="item-price">${item.price}</p>
+        </div>
+      `
+  });
+  return orderHtml;
+}
+
+//* Render user's order
+function renderOrder() {
+  document.getElementById("order-container").innerHTML = getOrderHtml();
+}
+
+
+
+
+//* SAVE OrderObject to Local Storage
+// if(currentOrderArray.length > 0){
+//   localStorage.setItem("order", JSON.stringify(currentOrderArray));
+// }
+
+// //* GET OrderObject from Local Storage
+// function getLocalStorage(object) {
+//   currentOrderArray = JSON.parse(localStorage.getItem("order"));
+// }
+
